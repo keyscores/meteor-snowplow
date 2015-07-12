356eaaa694c409801ca5fb333db219fc29756a57
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-07-12 19:05:13 -0400

    cleanup

--- server/main.js
+++ server/main.js
@@ -1,10 +1,6 @@
-// Test 
-// meteor add iron:router
-// meteor deploy experimental_meteor.meteor.com from burdon's macbook
-//meteor deploy kstracker.meteor.com from lucas's macbook
+// Deployed at tracker.keyscores.com with >mup deploy
 
-  Meteor.methods({
-
+Meteor.methods({
     reset: function() {
       Events.remove({});
     },
@@ -12,8 +8,7 @@
     update: function(ts) {
       Meteor._debug("TS: " + ts);
     }
-
-  });
+});
   
 
 
@@ -32,52 +27,12 @@ Router.route('track/:fileName', {where: 'server'})
         'Content-type': 'image/png',
         'Content-Disposition': "attachment; filename=" + this.params.fileName
     });
-
-	Events.insert({
-      path: PATH_FOR_YOUR_APP,
-      pathlocal: path.resolve('.')
-    }); 
-	
     fs.createReadStream(PATH_FOR_YOUR_APP+"/pixel/"+this.params.fileName).pipe(this.response);
     
-    //insert query string params
+//insert query string params
     Events.insert({
       query: this.params.query
     });   
   })
-  
- /*
 
-  // Serve actual file.
-  // TODO(burdon): Serve actual file.
-  var fs = Meteor.npmRequire('fs');
-  var file = fs.createReadStream('/public/cat.png'); 
- //fs.writeFile('message.txt', 'Hello new', function (err) {
-  //if (err) throw err;
-  //console.log('It\'s saved!');
-//});
-
-  // Iron Router
-  
-  Router.route('i', function() {
-    // Return valid image.
-    this.response.writeHead(200, {
-      'Content-type': 'image/png',
-      'Content-Disposition': "attachment; filename=" + this.params.path
-    });
-   // this.response.write('hello');
-    this.response.end('hello');
-    //fs.createReadStream(file).pipe(this.response);
-
-    //console.log('Router:', this.params.file, this.params.query);
-    Events.insert({
-      query: this.params.query
-    });    
-  }, { where: 'server' });
-
-
-//meteor's webapp request handler listener
-//console.log(WebApp.connectHandlers.listen());
-
-*/
 
