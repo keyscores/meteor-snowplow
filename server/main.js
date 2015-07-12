53a05c0812077e27a4c05e0c3e721143862eb5a9
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-07-12 18:56:27 -0400

    fixed serving pixel on production as well as localhost

--- server/main.js
+++ server/main.js
@@ -15,14 +15,16 @@
 
   });
   
+
+
+//SERVING FILES IN METEOR AN NEEDS ABSOLUTE PATH
+// Voodoo is needed to find the Meteor path whether online or in local dev.  Reference:http://stackoverflow.com/questions/17328807/what-is-the-meteor-server-side-path-to-public
 var fs = Npm.require('fs');
 var path = Npm.require('path');
+PATH_FOR_YOUR_APP = path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")
 
-console.log(path.resolve('.'));
-console.log(path.resolve('.', '../../../../../'));
-
-PATH_FOR_YOUR_APP = path.resolve('.', '../../../../../'); //NEEDS ABSOLUTE PATH using https://nodejs.org/api/path.html#path_path_basename_p_ext
-
+//USING IRON ROUTER TO SERVE STATIC FILES INSTEAD OF DEFAULT METEOR/PUBLIC, SO WE CAN GET THE QUERYSTRING PARAMS
+//Snowplow will fail silently if unsuccessful in downloading i pixel.
 //implementation from: http://stackoverflow.com/questions/31362749/meteor-js-serve-static-files-and-persist-query-string/31363579#31363579
 Router.route('track/:fileName', {where: 'server'})
   .get(function () {
@@ -31,8 +33,11 @@ Router.route('track/:fileName', {where: 'server'})
         'Content-Disposition': "attachment; filename=" + this.params.fileName
     });
 
-
-
+	Events.insert({
+      path: PATH_FOR_YOUR_APP,
+      pathlocal: path.resolve('.')
+    }); 
+	
     fs.createReadStream(PATH_FOR_YOUR_APP+"/pixel/"+this.params.fileName).pipe(this.response);
     
     //insert query string params
