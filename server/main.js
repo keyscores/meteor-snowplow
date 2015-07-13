50caacd64ca04937ee0bc3e09e5dfe3405e31d3d
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-07-13 11:37:34 -0400

    logging ip experiment

--- server/main.js
+++ server/main.js
@@ -34,3 +34,13 @@ Router.route('track/:fileName', {where: 'server'})
   })
 
 
+/* Lucas' experiment, logging all URLS called on meteor.
+
+WebApp.rawConnectHandlers.use(function (req, res, next) {
+  Log.info(req.connection.remoteAddress + ': ' + req.method + ' ' + req.url);
+  //Requests.insert({ip:req.connection.remoteAddress});
+  //Requests.insert({"ip":"req.connection.remoteAddress"});
+  next();
+});
+
+*/
